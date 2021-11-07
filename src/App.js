b7d66526e71cb2c1cc3621a9239f35299b975357
Elena <turonchek09@gmail.com>
2021-11-07 11:23:13 +0200

    task 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  let [isOpen, SetIsOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick = {() => SetIsOpen(!isOpen)}>Click me</button>
+      {isOpen && (
+        <h1>Hello!</h1>
+      )}
     </div>
   );
 }
