98f1b648e0d057dc8711fcf4c21003f1dbd66eea
Elena <turonchek09@gmail.com>
2021-11-07 12:29:57 +0200

    task 3

--- src/App.js
+++ src/App.js
@@ -1,36 +1,108 @@
 import React, {useState} from 'react';
 import './App.css';
 
-class App extends React.Component {
+
+export default class CustomerForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {name: ""};
 
-    this.onChange = this.onChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      customer: {
+        firstName: props.firstName,
+        lastName: props.lastName,
+        status: props.status
+      }
+    }
   }
-  onChange(e) {
-    var val = e.target.value;
-    this.setState({name: val});
-}
 
-  handleSubmit(e) {
-    e.preventDefault();
-    alert("Имя: " + this.state.name);
+  handleFirstNameChanged(event) {
+    var customer        = this.state.customer;
+    customer.firstName  = event.target.value;
+
+    this.setState({ customer: customer });
+  }
+
+  handleLastNameChanged(event) {
+    var customer      = this.state.customer;
+    customer.lastName = event.target.value;
+
+    this.setState({ customer: customer });
+  }
+
+  handleStatusChanged(event) {
+    var customer    = this.state.customer;
+    customer.status = event.target.value;
+
+    this.setState({ customer: customer });
+  }
+
+  handleButtonClicked() {
+    console.log(this.state.customer);
   }
 
   render() {
     return (
-        <form onSubmit={this.handleSubmit}>
-            <p>
-                <label>Введи свое имя:</label><br />
-                <input type="text" value={this.state.name} onChange={this.onChange}/>
-            </p>
-            <input type="submit" value="Отправить" />
-        </form>
+      <div>
+        <label>
+          First Name: 
+        </label>
+        <input type="text" value={this.state.customer.firstName} onChange={this.handleFirstNameChanged.bind(this)}/>
+        <br/>
+        <label>
+          Last Name:
+        </label>
+        <input type="text" value={this.state.customer.lastName} onChange={this.handleLastNameChanged.bind(this)}/>
+        <br/>
+        <label>
+          Status:
+        </label>
+        <select value={this.state.customer.status} onChange={this.handleStatusChanged.bind(this)}>
+          <option value="PENDING">
+            Pending
+          </option>
+          <option value="APPROVED">
+            Approved
+          </option>
+        </select>
+        <hr/>
+        <button onClick={this.handleButtonClicked.bind(this)}>
+          Save Record
+        </button>
+      </div>
     );
   }
 }
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {name: ""};
+
+//     this.onChange = this.onChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+//   onChange(e) {
+//     var val = e.target.value;
+//     this.setState({name: val});
+// }
+
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     alert("Имя: " + this.state.name);
+//   }
+
+//   render() {
+//     return (
+//         <form onSubmit={this.handleSubmit}>
+//             <p>
+//                 <label>Введи свое имя:</label><br />
+//                 <input type="text" value={this.state.name} onChange={this.onChange}/>
+//             </p>
+//             <input type="submit" value="Отправить" />
+//         </form>
+//     );
+//   }
+// }
 
 // function Welcome(props) {
 //   return <h1>Привет, {props.name}</h1>;
@@ -49,4 +121,4 @@ class App extends React.Component {
 //   );
 // }
 
-export default App;
+// export default App;
